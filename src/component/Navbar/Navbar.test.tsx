1bebcaebe7931605daca7c0c353460c691f4ab74
Milad Maleki <milad@Helpdesks-MacBook-Pro.local>
2022-04-16 14:54:24 +0800

    ready for deploy

--- src/component/Navbar/Navbar.test.tsx
+++ src/component/Navbar/Navbar.test.tsx
@@ -1,8 +1,6 @@
-import ReactDOM from 'react-dom';
-import { waitFor, render, screen, cleanup } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Navbar from './Navbar';
-import renderwithredux from '../../../renderWithRedux'
 
 afterEach(() => cleanup);
 
